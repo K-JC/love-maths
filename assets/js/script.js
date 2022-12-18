8b02d49402e4af5b9428d4632883cf0b5a3ad9b4
K-JC <kellytolfts93@hotmail.com>
2022-12-18 20:49:05 +0000

    display addtion questions to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
 
     }
+
+    runGame("addition");
 
 })
 
@@ -19,11 +21,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after users answer has been proccessed 
 */
 
-function runGame() {
+function runGame(gameType) {
 
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType} `);
+        throw `unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -42,8 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 }
 
 function displaySubtractQuestion() {
